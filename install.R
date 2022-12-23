c294e4a1836f69549fd1edc882f172d1cad955fd
lsaravia <lsaravia@users.noreply.github.com>
2022-12-23 16:46:54 -0300

    Update install.R

--- install.R
+++ install.R
@@ -1,7 +1,10 @@
-install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 # we recommend running this is a fresh R session or restarting your current session
 install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
-cmdstanr::install_cmdstan()
+library(cmdstanr)
+check_cmdstan_toolchain(fix = TRUE, quiet = TRUE)
+install_cmdstan(cores = 2)
+
+install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 
 install.packages(c("ggplot2","coda","mvtnorm","devtools","loo","dagitty","shape"))
 devtools::install_github("rmcelreath/rethinking")
