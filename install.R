a78840be008662cdfdb69b45e2c890ab82c64e8e
lsaravia <lsaravia@users.noreply.github.com>
2022-12-23 13:00:18 -0300

    Update install.R
    Add rstan cmdstanr rethinking and other packages

--- install.R
+++ install.R
@@ -1,1 +1,7 @@
-install.packages("ggplot2")
+install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+# we recommend running this is a fresh R session or restarting your current session
+install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+install_cmdstan()
+
+install.packages(c("ggplot2","coda","mvtnorm","devtools","loo","dagitty","shape"))
+devtools::install_github("rmcelreath/rethinking")
