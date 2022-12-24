3dae15852d91be7f0c2a2b32f8adcafdf72eafe5
lsaravia <lsaravia@users.noreply.github.com>
2022-12-23 22:15:59 -0300

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,18 @@
-#install.packages(c("StanHeaders","rstan"),type="source")
+#install.packages(c("StanHeaders","rstan"),type="source")dotR <- file.path(Sys.getenv("HOME"), ".R")
+
+if (!file.exists(dotR)) dir.create(dotR)
+M <- file.path(dotR, "Makevars")
+if (!file.exists(M)) file.create(M)
+cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
+    "CXX14=g++", # or clang++ but you may need a version postfix
+    file = M, sep = "\n", append = TRUE)
+dotR <- file.path(Sys.getenv("HOME"), ".R")
+if (!file.exists(dotR)) dir.create(dotR)
+M <- file.path(dotR, "Makevars")
+if (!file.exists(M)) file.create(M)
+cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
+    "CXX14=g++", # or clang++ but you may need a version postfix
+    file = M, sep = "\n", append = TRUE)
 install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 
 #install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
